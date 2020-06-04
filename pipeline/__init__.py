db2ef6ae3a95899a8b9aef5e318f09c5a234f57e
Thinh Nguyen <thinh@vathes.com>
2020-06-04 15:02:32 -0500

    add prototype generic "fetch_nwb" method

--- pipeline/__init__.py
+++ pipeline/__init__.py
@@ -1,6 +1,9 @@
 import os
+import datajoint as dj
+import pathlib
 
 os.environ['DJ_SUPPORT_ADAPTED_TYPES'] = 'TRUE'
 os.environ['DJ_SUPPORT_FILEPATH_MANAGEMENT'] = 'TRUE'
 
-
+dj.config['stores']['nwb_store']['stage'] = pathlib.Path(dj.config['stores']['nwb_store']['stage']).resolve().as_posix()
+dj.config['stores']['nwb_store']['location'] = pathlib.Path(dj.config['stores']['nwb_store']['location']).resolve().as_posix()
